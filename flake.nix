5360314d76f0e4c0fe6abe9bd68d0d02cfe6d865
Lektor <denislour@gmail.com>
2026-05-17 00:20:32 +0700

    init: scaffold NixOS flake project

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in { };
+}
