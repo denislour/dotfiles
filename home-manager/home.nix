6f29e947db8d6e90384eb72248a54c7bc8d99241
Lektor <denislour@gmail.com>
2026-05-18 15:22:48 +0700

    feat: rust CLI tools (bat, eza, fd, rg, dust, procs, btm, delta, sd, zoxide) + bash aliases

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,6 +6,16 @@
 
   home.packages = with pkgs; [
     fastfetch
+    # Rust CLI tools — replacements for coreutils
+    bat         # cat with syntax highlighting
+    eza         # ls with icons + git status
+    fd          # find replacement
+    ripgrep     # grep replacement
+    du-dust     # du replacement
+    procs       # ps replacement
+    bottom      # htop replacement
+    delta       # diff with syntax highlighting
+    sd          # sed replacement
   ];
 
   programs.git = {
@@ -36,11 +46,27 @@
     };
   };
 
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
   programs.bash = {
     enable = true;
     bashrcExtra = ''
-      alias ll='ls -la'
-      alias la='ls -A'
+      # Rust CLI aliases
+      alias ls='eza --icons'
+      alias ll='eza -la --icons'
+      alias la='eza -A --icons'
+      alias lt='eza -T --icons'
+      alias cat='bat'
+      alias grep='rg'
+      alias find='fd'
+      alias ps='procs'
+      alias du='dust'
+      alias diff='delta'
+      alias sed='sd'
+      alias top='btm'
       alias v='vim'
       alias g='git'
     '';
