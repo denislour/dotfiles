e04770f1d446e95687538470965042b6edc2a6ed
Lektor <denislour@gmail.com>
2026-05-19 01:41:13 +0700

    chore: remove icons from starship

--- home/programs/starship.nix
+++ home/programs/starship.nix
@@ -5,17 +5,15 @@
     settings = {
       add_newline = true;
       character = {
-        success_symbol = "[➜](bold.green)";
-        error_symbol = "[➜](bold.red)";
+        success_symbol = "[>](bold.green)";
+        error_symbol = "[>](bold.red)";
       };
       nix_shell = {
-        symbol = "❄️ ";
-        format = "via [$symbol$state( \\($name\\))]($style) ";
+        format = "via [$state( \\($name\\))]($style) ";
       };
       git_branch = {
-        format = "on [$symbol$branch(:$remote_branch)]($style) ";
+        format = "on [$branch(:$remote_branch)]($style) ";
       };
-      git_status = { style = "bold yellow"; };
       directory = {
         truncation_length = 3;
         style = "bold cyan";
