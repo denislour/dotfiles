4b833bba2cd1feb7aa72d7a4d4f9e4a078da0426
Lektor <denislour@gmail.com>
2026-05-19 00:01:10 +0700

    fix: remove ghostty theme stylix (file not found, autoEnable=false)

--- home/programs/ghostty.nix
+++ home/programs/ghostty.nix
@@ -6,7 +6,6 @@
     settings = {
       font-size = 11;
       font-family = "JetBrainsMono Nerd Font";
-      theme = "stylix";
       window-decoration = false;
       font-feature = [
         "-liga"
