56046a880b447a2b7280d46f7f09e7e79d75553a
Lektor <denislour@gmail.com>
2026-05-18 10:58:54 +0700

    fix: add polkit_gnome as systemd service with correct package name

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -15,6 +15,21 @@
     pulse.enable = true;
   };
 
+  # Polkit authentication agent (graphical)
+  systemd.user.services.polkit-gnome = {
+    description = "PolicyKit Authentication Agent";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = "1s";
+      TimeoutStopSec = "10s";
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     kitty
     waybar
@@ -25,5 +40,6 @@
     slurp
     swappy
     thunar
+    polkit_gnome
   ];
 }
