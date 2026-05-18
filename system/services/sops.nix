62ee02fac5a56bbfbc5e88fec6fdcfa9392a186a
Lektor <denislour@gmail.com>
2026-05-19 02:34:57 +0700

    fix: use manual age-keygen (generateKey=false)

--- system/services/sops.nix
+++ system/services/sops.nix
@@ -4,5 +4,5 @@
   sops.defaultSopsFile = "${self}/secrets/secrets.yaml";
   sops.defaultSopsFormat = "yaml";
 
-  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  sops.age.keyFile = "/home/jake/.config/sops/age/keys.txt";
 }
