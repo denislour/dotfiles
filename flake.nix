10df9cccd10a2d201bb7d2d00b1000cc4876781e
Lektor <denislour@gmail.com>
2026-05-19 02:10:07 +0700

    feat: add sops-nix for secrets management

--- flake.nix
+++ flake.nix
@@ -28,9 +28,14 @@
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, disko, stylix, niri, noctalia, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, disko, stylix, niri, noctalia, sops-nix, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -51,11 +56,13 @@
         modules = [
           stylix.nixosModules.stylix
           disko.nixosModules.disko
+          inputs.sops-nix.nixosModules.sops
           ./hosts/my-vm/disk-config.nix
           ./hosts/my-vm/configuration.nix
           ./system/packages.nix
           ./system/environment.nix
           ./system/services/greetd.nix
+          ./system/services/sops.nix
           ./system/common.nix
           ./system/programs/stylix.nix
           ./system/programs/xdg-portal.nix
---
+++ system/services/sops.nix
@@ -0,0 +1,8 @@
+{ self, ... }:
+
+{
+  sops.defaultSopsFile = "${self}/secrets/secrets.yaml";
+  sops.defaultSopsFormat = "yaml";
+
+  sops.age.keyFile = "/home/jake/.config/sops/age/keys.txt";
+}
