2ae2eb7a7e0fd2262651fe36fba1c681ebb700fd
Lektor <denislour@gmail.com>
2026-05-19 01:57:58 +0700

    fix: add backupFileExtension to handle clobbered config files

--- flake.nix
+++ flake.nix
@@ -64,6 +64,7 @@
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.jake = import ./hosts/my-vm/home.nix;
           }
