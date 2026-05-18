c3a377ebce02eb0584c604c853047348140e67fe
Lektor <denislour@gmail.com>
2026-05-18 22:57:07 +0700

    feat: add home-manager entry point (hosts/my-vm/home.nix)

---
+++ hosts/my-vm/home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "jake";
+  home.homeDirectory = "/home/jake";
+  home.stateVersion = "25.05";
+
+  imports = [
+    ../../home/niri
+    ../../home/programs/bash.nix
+    ../../home/programs/ghostty.nix
+    ../../home/programs/git.nix
+    ../../home/programs/yazi.nix
+    ../../home/programs/zoxide.nix
+  ];
+
+  home.packages = with pkgs; [
+    fastfetch
+    bat eza fd ripgrep dust procs bottom delta sd
+    just
+  ];
+
+  programs.home-manager.enable = true;
+}
