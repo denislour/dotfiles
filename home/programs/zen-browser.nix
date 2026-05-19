25db83f35cfd15221ca6c0b0a54a88a48c8820f5
Lektor <denislour@gmail.com>
2026-05-19 23:00:36 +0700

    refactor: configure Zen Browser with Chrome/Firefox-style horizontal tabs

--- home/programs/zen-browser.nix
+++ home/programs/zen-browser.nix
@@ -20,6 +20,10 @@
         "browser.uidensity" = 1;
         "widget.dmabuf.force-enabled" = true;
         "full-screen-api.ignore-widgets" = true;
+        "zen.tabs.vertical" = false;
+        "zen.theme.essentials-header" = false;
+        "zen.view.sidebar-expanded" = false;
+        "zen.theme.accent-color" = "#89b4fa";
       };
 
       search = {
@@ -33,8 +37,9 @@
       };
 
       userChrome = ''
-        #tabbrowser-tabs { visibility: collapse !important; }
-        #sidebar-box { --zen-toolbox-width: 35px; }
+        /* Chrome/Firefox style - hide Zen sidebar, show horizontal tabs */
+        #sidebar-box { display: none !important; }
+        #sidebar-splitter { display: none !important; }
       '';
 
       userContent = ''
