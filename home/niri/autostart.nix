fd01a35624ff64f21f5705a6aa4e8d9e7c13b399
Lektor <denislour@gmail.com>
2026-05-20 00:06:38 +0700

    fix: add xdg-desktop-portal-gtk to Niri autostart for file dialog

--- home/niri/autostart.nix
+++ home/niri/autostart.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.niri.settings.spawn-at-startup = [
@@ -8,5 +8,8 @@
 
     { command = [ "sh" "-c" "wl-paste --type text --watch cliphist store" ]; }
     { command = [ "sh" "-c" "wl-paste --type image --watch cliphist store" ]; }
+
+    # Đảm bảo GTK portal backend chạy để Zed (và các app khác) có thể mở folder/file dialog
+    { command = [ "${pkgs.xdg-desktop-portal-gtk}/libexec/xdg-desktop-portal-gtk" ]; }
   ];
 }
