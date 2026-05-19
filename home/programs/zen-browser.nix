786d87e8a24148de104057061ad963736c9c2f28
Lektor <denislour@gmail.com>
2026-05-19 18:13:15 +0700

    fix: explicitly set zen-browser package from flake

--- home/programs/zen-browser.nix
+++ home/programs/zen-browser.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   imports = [
@@ -7,6 +7,7 @@
 
   programs.zen-browser = {
     enable = true;
+    package = inputs.zen.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
     profiles.jake = {
       id = 0;
