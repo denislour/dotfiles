15e017b7e11488d32e6f71058a50c75ab3e453e8
Lektor <denislour@gmail.com>
2026-05-19 18:39:48 +0700

    feat: auto-start SSH agent in zsh

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -5,6 +5,12 @@
     autocd = true;
     enableCompletion = true;
     initContent = ''
+      # SSH agent (auto-start once per login)
+      if [ -z "$SSH_AUTH_SOCK" ]; then
+        eval "$(ssh-agent -s)" > /dev/null 2>&1
+        ssh-add ~/.ssh/id_ed25519 2>/dev/null
+      fi
+
       alias history='atuin history'
       alias ls='eza --icons'
       alias ll='eza -la --icons'
