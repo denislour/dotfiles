7a1a49ab3900a48ed3a8e3f5472c8273a936c964
Lektor <denislour@gmail.com>
2026-05-20 00:07:48 +0700

    chore: remove comments

--- home/niri/autostart.nix
+++ home/niri/autostart.nix
@@ -9,7 +9,6 @@
     { command = [ "sh" "-c" "wl-paste --type text --watch cliphist store" ]; }
     { command = [ "sh" "-c" "wl-paste --type image --watch cliphist store" ]; }
 
-    # Đảm bảo GTK portal backend chạy để Zed (và các app khác) có thể mở folder/file dialog
     { command = [ "${pkgs.xdg-desktop-portal-gtk}/libexec/xdg-desktop-portal-gtk" ]; }
   ];
 }
