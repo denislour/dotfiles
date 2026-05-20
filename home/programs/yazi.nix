573f938d0087457bc0d63bdc6203caca9988b188
Lektor <denislour@gmail.com>
2026-05-20 14:05:14 +0700

    feat(yazi): add linemode keymaps for sizes mtime permissions

--- home/programs/yazi.nix
+++ home/programs/yazi.nix
@@ -15,5 +15,13 @@
         image_filter = "lanczos3";
       };
     };
+
+    keymap = {
+      manager.prepend_keymap = [
+        { on = "g n"; run = "linemode \"size\""; desc = "Show file sizes"; }
+        { on = "g m"; run = "linemode \"mtime\""; desc = "Show modification time"; }
+        { on = "g p"; run = "linemode \"permissions\""; desc = "Show permissions"; }
+      ];
+    };
   };
 }
