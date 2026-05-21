757221b6b612ddf7ab54f1d850ef7096fd6c7d24
Lektor <denislour@gmail.com>
2026-05-21 23:27:19 +0700

    fix(astronvim): neogit transparent background

--- home/programs/astronvim/lua/plugins/nightfox.lua
+++ home/programs/astronvim/lua/plugins/nightfox.lua
@@ -7,6 +7,13 @@ return {
       vim.cmd.colorscheme("carbonfox")
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+      vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+      vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
+      vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NeogitStatusHeader", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NeogitStatusTitle", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NeogitDiffHeader", { bg = "none" })
     end,
   },
 }
