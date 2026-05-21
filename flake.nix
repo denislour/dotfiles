be53f15643c208d74c8162e4756323a06af8e9cd
Lektor <denislour@gmail.com>
2026-05-21 23:36:59 +0700

    fix: change backup extension to hm-backup to avoid conflicts

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "backup";
+            home-manager.backupFileExtension = "hm-backup";
             home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.jake = import ./hosts/my-vm/home.nix;
           }
