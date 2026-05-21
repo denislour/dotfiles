43d7c5fe221fd615cf71493578dbdfe1a4c85bbf
Lektor <denislour@gmail.com>
2026-05-22 00:02:46 +0700

    feat(astronvim): add leader-yy keymap for yazi

---
+++ home/programs/astronvim/lua/plugins/yazi.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>yy"] = { "<Cmd>Yazi<CR>", desc = "Open yazi" },
+        },
+      },
+    },
+  },
+}
