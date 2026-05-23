561257ee0fb4a58c17afb11ac8956f8637d73dbd
Lektor <denislour@gmail.com>
2026-05-23 23:50:12 +0700

    fix: add QT_QPA_PLATFORMTHEME=qt5ct for Qt app theming

--- home/niri/settings.nix
+++ home/niri/settings.nix
@@ -77,6 +77,7 @@
         MOZ_ENABLE_WAYLAND = "1";
         NIXOS_OZONE_WL = "1";
         QT_QPA_PLATFORM = "wayland";
+        QT_QPA_PLATFORMTHEME = "qt5ct";
         QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
         ELECTRON_OZONE_PLATFORM_HINT = "auto";
         XDG_SESSION_TYPE = "wayland";
