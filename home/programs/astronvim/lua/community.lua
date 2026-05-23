af05c86d76f9994e97e9c38aaddf88e40845f04f
Lektor <denislour@gmail.com>
2026-05-24 00:30:34 +0700

    fix: correct markdown-preview-nvim import path

--- home/programs/astronvim/lua/community.lua
+++ home/programs/astronvim/lua/community.lua
@@ -4,7 +4,7 @@ return {
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.ruby" },
-  { import = "astrocommunity.markdown-preview.markdown-preview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
 }
