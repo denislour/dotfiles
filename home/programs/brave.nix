17401381842aab53f2971d64ae9f04a8894385b1
Lektor <denislour@gmail.com>
2026-06-01 01:55:28 +0700

    clean: remove xdg.mimeApps, keep only brave essentials

--- home/programs/brave.nix
+++ home/programs/brave.nix
@@ -1,6 +1,4 @@
 { pkgs, ... }: {
-  xdg.mimeApps.enable = true;
-
   programs.brave = {
     enable = true;
     commandLineArgs = [
@@ -8,18 +6,5 @@
       "--enable-features=WebContentsForceDark:inversion_method/cielab_based/image_behavior/none"
       "--ozone-platform-hint=auto"
     ];
-  };
-
-  xdg.mimeApps.defaultApplications = {
-    "x-scheme-handler/http" = "brave-browser.desktop";
-    "x-scheme-handler/https" = "brave-browser.desktop";
-    "x-scheme-handler/chrome" = "brave-browser.desktop";
-    "text/html" = "brave-browser.desktop";
-    "application/x-extension-htm" = "brave-browser.desktop";
-    "application/x-extension-html" = "brave-browser.desktop";
-    "application/x-extension-shtml" = "brave-browser.desktop";
-    "application/xhtml+xml" = "brave-browser.desktop";
-    "application/x-extension-xhtml" = "brave-browser.desktop";
-    "application/x-extension-xht" = "brave-browser.desktop";
   };
 }
