ab2db010f9f213e30e10fc7bf8efd4da7274dbac
Lektor <denislour@gmail.com>
2026-06-01 01:00:37 +0700

    chore: swap falkon for qutebrowser with light theme config

--- home/packages.nix
+++ home/packages.nix
@@ -1,13 +1,13 @@
 { pkgs, lib, ... }:
 
 let
-  falkonLight = pkgs.writeShellScriptBin "falkon" ''
+  quteLight = pkgs.writeShellScriptBin "qutebrowser" ''
     export QT_STYLE_OVERRIDE=fusion
-    exec ${lib.getExe pkgs.kdePackages.falkon} "$@"
+    exec ${lib.getExe pkgs.qutebrowser} "$@"
   '';
 in {
   home.packages = with pkgs; [
-    falkonLight
+    quteLight
     cliphist
     dust
     glow
@@ -25,4 +25,23 @@ in {
     worker-build
     sqlite
   ];
+
+  home.file.".config/qutebrowser/config.py".text = ''
+    c.colors.webpage.darkmode.enabled = False
+    c.colors.webpage.preferred_color_scheme = "light"
+    c.colors.tabs.bg = "#f5f5f5"
+    c.colors.tabs.fg = "#333333"
+    c.colors.tabs.selected.bg = "#ffffff"
+    c.colors.tabs.selected.fg = "#000000"
+    c.colors.statusbar.normal.bg = "#f5f5f5"
+    c.colors.statusbar.normal.fg = "#333333"
+    c.colors.completion.fg = "#333333"
+    c.colors.completion.match.fg = "#0066cc"
+    c.colors.completion.category.bg = "#e0e0e0"
+    c.colors.completion.category.fg = "#000000"
+    c.colors.completion.item.selected.bg = "#d0d0d0"
+    c.colors.completion.item.selected.fg = "#000000"
+    c.fonts.default_size = "14pt"
+    c.fonts.monospace = "JetBrainsMono Nerd Font"
+  '';
 }
