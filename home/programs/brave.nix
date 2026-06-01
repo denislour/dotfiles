42d37036b722309daba92768d4d059b6b4ef996f
Lektor <denislour@gmail.com>
2026-06-02 02:42:20 +0700

    feat: set brave zoom to 125%

--- home/programs/brave.nix
+++ home/programs/brave.nix
@@ -5,6 +5,7 @@
       "--force-dark-mode"
       "--enable-features=WebContentsForceDark:inversion_method/cielab_based/image_behavior/none"
       "--ozone-platform-hint=auto"
+      "--force-device-scale-factor=1.25"
     ];
   };
 }
