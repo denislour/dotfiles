45a4a9d7602d682c4bc1c40c1364e791555741d9
Lektor <denislour@gmail.com>
2026-06-02 02:37:45 +0700

    feat: add mint lsp via mint tool ls

--- home/programs/astronvim/lua/plugins/mint.lua
+++ home/programs/astronvim/lua/plugins/mint.lua
@@ -3,4 +3,25 @@ return {
     "IrenejMarc/vim-mint",
     ft = { "mint" },
   },
+  {
+    "AstroNvim/astrolsp",
+    opts = {
+      servers = {
+        "mint",
+      },
+      config = {
+        mint = {
+          cmd = { "mint", "tool", "ls" },
+          filetypes = { "mint" },
+          root_markers = { "mint.json", ".git" },
+        },
+      },
+      formatting = {
+        format_on_save = {
+          enabled = true,
+          ignore_filetypes = { "mint" },
+        },
+      },
+    },
+  },
 }
