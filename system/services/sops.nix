337e6034d5ada55f2cda356e8547e621f2691c27
Lektor <denislour@gmail.com>
2026-06-03 23:12:26 +0700

    fix: use config.users.users.jake.home instead of hardcoded path in sops.nix

--- system/services/sops.nix
+++ system/services/sops.nix
@@ -1,10 +1,10 @@
-{ self, ... }:
+{ config, self, ... }:
 
 {
   sops.defaultSopsFile = "${self}/secrets/secrets.yaml";
   sops.defaultSopsFormat = "yaml";
 
-  sops.age.keyFile = "/home/jake/.config/sops/age/keys.txt";
+  sops.age.keyFile = "${config.users.users.jake.home}/.config/sops/age/keys.txt";
 
   sops.secrets = {
     brave_search_api_key = { owner = "jake"; mode = "0444"; };
