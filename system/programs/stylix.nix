bd32883c029456091d92647b83cb2070c2f90d17
Lektor <denislour@gmail.com>
2026-06-03 20:59:53 +0700

    add cool base16 scheme (catppuccin mocha)

--- system/programs/stylix.nix
+++ system/programs/stylix.nix
@@ -6,6 +6,25 @@
     autoEnable = false;
     polarity = "dark";
 
+    base16Scheme = {
+      base00 = "1e1e2e";
+      base01 = "2e2e3e";
+      base02 = "363647";
+      base03 = "585b70";
+      base04 = "8c8fa1";
+      base05 = "c6d0f5";
+      base06 = "e6e9f0";
+      base07 = "f2f3f5";
+      base08 = "f28fad";
+      base09 = "f8bd96";
+      base0A = "f9e2af";
+      base0B = "a6e3a1";
+      base0C = "89dceb";
+      base0D = "8caaee";
+      base0E = "caa7f0";
+      base0F = "f5c2e7";
+    };
+
     cursor = {
       package = pkgs.banana-cursor;
       name = "Banana";
