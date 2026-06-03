2d4ff1dcee8d06c828d00f2675bea74fabe90106
Lektor <denislour@gmail.com>
2026-06-03 19:14:39 +0700

    remove auto-login from lightdm

--- system/x11/display-manager.nix
+++ system/x11/display-manager.nix
@@ -3,10 +3,6 @@
 {
   services.xserver.displayManager.lightdm = {
     enable = true;
-    autoLogin = {
-      enable = true;
-      user = "jake";
-    };
   };
 
   services.xserver.displayManager.defaultSession = "none+i3";
