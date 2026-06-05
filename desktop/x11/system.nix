baec40d2c83f5b07c939e8ab2138089e6dc29e5f
Lektor <denislour@gmail.com>
2026-06-06 00:59:39 +0700

    fix: replace bspwm module with custom displayManager.session

--- desktop/x11/system.nix
+++ desktop/x11/system.nix
@@ -1,8 +1,30 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  bspwmrc = pkgs.writeShellScript "bspwmrc" ''
+    ${pkgs.sxhkd}/bin/sxhkd -c ~/.config/sxhkd/sxhkdrc &
+    ${pkgs.picom}/bin/picom -b
+    ${pkgs.dunst}/bin/dunst &
+    ${pkgs.feh}/bin/feh --bg-fill '#2d353b'
+    ${pkgs.polybar}/bin/polybar main &
+    ${pkgs.lxqt.lxqt-policykit}/bin/lxqt-policykit-agent &
+    bspc monitor -d 1 2 3 4
+    bspc config border_width 0
+    bspc config window_gap 4
+  '';
+in
+{
   services.xserver.desktopManager.xterm.enable = false;
-  services.xserver.windowManager.bspwm.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
-  services.displayManager.defaultSession = "none+bspwm";
+  services.displayManager.defaultSession = "bspwm";
+  services.displayManager.session = [{
+    manage = "window";
+    name = "bspwm";
+    start = ''
+      ${bspwmrc} &
+      waitPID=$!
+      wait "$waitPID"
+    '';
+  }];
 
   services.xserver.videoDrivers = [ "vmware" "modesetting" ];
 
