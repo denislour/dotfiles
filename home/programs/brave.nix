31e211749c5d66f63e283ea4e41256e95f1d78b0
Lektor <denislour@gmail.com>
2026-06-05 19:39:08 +0700

    fix: disable UseAlternateProfileLock for brave to avoid hostname lock conflicts

--- home/programs/brave.nix
+++ home/programs/brave.nix
@@ -5,6 +5,7 @@
       "--force-dark-mode"
       "--enable-features=WebContentsForceDark:inversion_method/cielab_based/image_behavior/none"
       "--force-device-scale-factor=1.25"
+      "--disable-features=UseAlternateProfileLock"
     ];
   };
 }
