62b12b72de072caa41ac59a4ed715afe12cbbda1
Lektor <denislour@gmail.com>
2026-06-05 19:20:15 +0700

    fix: remove ozone-platform-hint from brave for cross-desktop compat

--- home/programs/brave.nix
+++ home/programs/brave.nix
@@ -4,7 +4,6 @@
     commandLineArgs = [
       "--force-dark-mode"
       "--enable-features=WebContentsForceDark:inversion_method/cielab_based/image_behavior/none"
-      "--ozone-platform-hint=auto"
       "--force-device-scale-factor=1.25"
     ];
   };
