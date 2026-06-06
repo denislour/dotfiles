b2d15f7c72fda2d2e9d140ee16cfb99106d92581
Lektor <denislour@gmail.com>
2026-06-07 01:01:22 +0700

    feat: enable login saving in librewolf

--- home/programs/librewolf.nix
+++ home/programs/librewolf.nix
@@ -1,3 +1,14 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ librewolf ];
+{
+  programs.librewolf = {
+    enable = true;
+    profiles.default = {
+      isDefault = true;
+      settings = {
+        "signon.rememberSignons" = true;
+        "network.cookie.lifetimePolicy" = 0;
+        "privacy.resistFingerprinting" = false;
+        "privacy.firstparty.isolate" = false;
+      };
+    };
+  };
 }
