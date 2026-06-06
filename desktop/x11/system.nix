390726ae9af6d7e62524418b3b92a5c078eb2027
Lektor <denislour@gmail.com>
2026-06-07 01:24:54 +0700

    fix: use desktopManager.session for openbox instead of displayManager session

--- desktop/x11/system.nix
+++ desktop/x11/system.nix
@@ -1,14 +1,14 @@
 { pkgs, lib, ... }:
 
 {
-  services.displayManager.defaultSession = lib.mkDefault "none+openbox";
+  services.displayManager.defaultSession = lib.mkDefault "openbox";
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
 
-  services.xserver.displayManager.session = [{
+  services.xserver.desktopManager.session = [{
     name = "openbox";
-    manage = "window";
     start = ''
+      source /home/jake/.config/openbox/autostart
       exec ${pkgs.openbox}/bin/openbox
     '';
   }];
