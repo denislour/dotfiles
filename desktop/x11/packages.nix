ac622a7cfde28ad545b0ff07c5a0f8cd3fc4e892
Lektor <denislour@gmail.com>
2026-06-07 14:30:12 +0700

    refactor: remove unused packages from x11

--- desktop/x11/packages.nix
+++ desktop/x11/packages.nix
@@ -1,16 +1,8 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    brightnessctl
     feh
-    lxqt.lxqt-policykit
     maim
     networkmanagerapplet
-    pavucontrol
     playerctl
-    redshift
-    slop
-    wireplumber
-    xclip
-    xsel
   ];
 }
