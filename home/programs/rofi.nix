e94b5d383988a673b9c78396f2bad8a5a7e91f7f
Lektor <denislour@gmail.com>
2026-06-07 22:43:04 +0700

    fix: use programs.rofi.extraConfig to avoid overwriting stylix theme

--- home/programs/rofi.nix
+++ home/programs/rofi.nix
@@ -1,14 +1,15 @@
 {
-  xdg.configFile."rofi/config.rasi".text = ''
-    configuration {
-      modi:                       "drun,run,window";
-      show-icons:                 true;
-      icon-theme:                 "Papirus";
-      display-drun:               "";
-      display-run:                "";
-      display-window:             "";
-      drun-display-format:        "{name}";
-      font:                       "JetBrainsMonoNL Nerd Font 12";
-    }
-  '';
+  programs.rofi = {
+    enable = true;
+    extraConfig = {
+      modi = "drun,run,window";
+      show-icons = true;
+      icon-theme = "Papirus";
+      display-drun = "";
+      display-run = "";
+      display-window = "";
+      drun-display-format = "{name}";
+      font = "JetBrainsMonoNL Nerd Font 12";
+    };
+  };
 }
