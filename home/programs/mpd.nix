962b16b0dce57f70c75a3c90288b39a63c3fb41a
Lektor <denislour@gmail.com>
2026-06-07 21:40:28 +0700

    fix: use absolute paths for mpd, remove heavy visualizer from ncmpcpp

--- home/programs/mpd.nix
+++ home/programs/mpd.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }:
+
+let
+  homeDir = config.home.homeDirectory;
+in {
   services.mpd = {
     enable = true;
-    musicDirectory = "%h/Music";
-    playlistDirectory = "%h/.config/mpd/playlists";
-    dbFile = "%h/.config/mpd/database";
+    musicDirectory = "${homeDir}/Music";
+    playlistDirectory = "${homeDir}/.config/mpd/playlists";
+    dbFile = "${homeDir}/.config/mpd/database";
     extraConfig = ''
       audio_output {
         type "pulse"
@@ -14,6 +18,5 @@
 
   programs.ncmpcpp = {
     enable = true;
-    package = pkgs.ncmpcpp.override { visualizerSupport = true; };
   };
 }
