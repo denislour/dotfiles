cc7390c81d4a7224642c8d180015b02b5e89bec5
Lektor <denislour@gmail.com>
2026-06-07 12:58:16 +0700

    fix: remove ly, restore lightdm

--- desktop/x11/system.nix
+++ desktop/x11/system.nix
@@ -1,25 +1,9 @@
 { pkgs, lib, ... }:
 
 {
-  services.displayManager = {
-    ly = {
-      enable = true;
-      settings = {
-        bg = "1e1e2e";
-        fg = "cdd6f4";
-        asterisk = "◆";
-        clear_term = true;
-        lang = "us";
-        save = false;
-        load = true;
-        min_refresh_delta = 50;
-        xauth_cmd = "${pkgs.xauth}/bin/xauth";
-      };
-    };
-    defaultSession = lib.mkDefault "bspwm";
-  };
+  services.displayManager.defaultSession = lib.mkDefault "bspwm";
 
-  environment.etc."ly/setup.sh".source = "${pkgs.ly}/etc/setup.sh";
+  services.xserver.displayManager.lightdm.enable = true;
 
   services.xserver.desktopManager.session = [{
     name = "bspwm";
--- hosts/my-vm-x11/configuration.nix
+++ hosts/my-vm-x11/configuration.nix
@@ -4,4 +4,5 @@
   networking.hostName = "my-vm-x11";
 
   services.xserver.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
 }
