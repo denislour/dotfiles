1908ad3fb927199254d0743f2d5c938121bb4b5a
Lektor <denislour@gmail.com>
2026-06-12 21:56:53 +0700

    fix: simplify zellij config, remove broken default_shell and compact_bar

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -6,13 +6,10 @@
 
     plugins {
         tab_bar location="zellij:tab-bar"
-        compact_bar location="zellij:compact-bar"
     }
     pane_frames false
     simplified_ui true
     show_startup_tips false
-    default_shell "${pkgs.zsh}/bin/zsh"
-    scrollback_buffer_size 10000
 
     keybinds {
         unbind "Ctrl + o"
