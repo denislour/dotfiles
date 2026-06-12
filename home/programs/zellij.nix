74d64068a7686babfa43981649585780c4dfff65
Lektor <denislour@gmail.com>
2026-06-12 21:43:19 +0700

    style: simplify zellij with compact layout and minimal ui

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -9,9 +9,11 @@
       show_startup_tips = false;
       pane_frames = false;
       default_shell = "${pkgs.zsh}/bin/zsh";
+      default_layout = "compact";
       plugins = {
         tab_bar = "zellij:tab-bar";
         compact_bar = "zellij:compact-bar";
+        status_bar = "zellij:status-bar";
       };
       keybinds = {
         unbind = [ "Ctrl o" ];
