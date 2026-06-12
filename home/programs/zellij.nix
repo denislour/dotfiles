a141bbecc42aeaff1a5259a9b67b41a071905673
Lektor <denislour@gmail.com>
2026-06-12 22:06:23 +0700

    fix: clean zellij ui - remove bottom elements, explicit default layout

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -3,13 +3,14 @@
 
   xdg.configFile."zellij/config.kdl".text = ''
     theme "catppuccin-mocha"
+    default_layout "default"
+    pane_frames false
+    simplified_ui true
+    show_startup_tips false
 
     plugins {
         tab_bar location="zellij:tab-bar"
     }
-    pane_frames false
-    simplified_ui true
-    show_startup_tips false
 
     keybinds {
         unbind "Ctrl o"
