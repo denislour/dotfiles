91b528767de88bd7d06de777b4e703d4eb696b6c
Lektor <denislour@gmail.com>
2026-06-12 22:28:17 +0700

    feat: enable auto-login for jake on my-vm-x11

--- hosts/my-vm-x11/configuration.nix
+++ hosts/my-vm-x11/configuration.nix
@@ -1,8 +1,14 @@
-{ ... }: {
+{ lib, ... }:
+{
   imports = [ ../../system/host-base.nix ];
 
   networking.hostName = "my-vm-x11";
 
   services.xserver.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
+
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "jake";
+  };
 }
