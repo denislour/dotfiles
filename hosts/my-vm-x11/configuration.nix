bd4a5d1a70c3c6e7c84759ff2c3de0b676bfd0af
Lektor <denislour@gmail.com>
2026-06-20 13:27:19 +0700

    fix: permit librewolf insecure package (no active committer)

--- hosts/my-vm-x11/configuration.nix
+++ hosts/my-vm-x11/configuration.nix
@@ -1,5 +1,7 @@
-{ lib, ... }:
-{
+{ ... }: {
+  nixpkgs.config.permittedInsecurePackages = [
+    "librewolf-151.0.2-1"
+  ];
   imports = [ ../../system/host-base.nix ];
 
   networking.hostName = "my-vm-x11";
