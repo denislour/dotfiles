bd34256a39ec32c4535c81bd6d0e3ca70c2faa24
Lektor <denislour@gmail.com>
2026-06-27 21:01:52 +0700

    feat: add VS Code with --disable-gpu for VMware

--- home/packages.nix
+++ home/packages.nix
@@ -1,16 +1,32 @@
-{ pkgs, ... }: {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  vscodeWithFlags = pkgs.symlinkJoin {
+    name = "vscode-with-flags";
+    paths = [pkgs.vscode];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      rm -f $out/bin/code
+      makeWrapper ${pkgs.vscode}/bin/code $out/bin/code \
+        --add-flags "--disable-gpu"
+    '';
+  };
+in {
   home.packages = with pkgs; [
     dust
     glow
     just
     procs
     sd
+    vscodeWithFlags
     trunk
     crystal
     shards
     mint
     ameba
-    xrandr
     wrangler
     worker-build
     sqlite
